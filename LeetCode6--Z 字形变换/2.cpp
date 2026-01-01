ae41afa04a58e7d32ff5c002a54a31137de96895
harvest0623 <3367741939@qq.com>
2026-01-02 01:26:26 +0800

    LeetCode6

---
+++ LeetCode6--Z 字形变换/2.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if (numRows < 2)
+            return s;
+        vector<string> rows(numRows);
+        int i = 0, flag = -1;
+        for (char c : s) {
+            rows[i].push_back(c);
+            if (i == 0 || i == numRows - 1)
+                flag = -flag;
+            i += flag;
+        }
+        string res;
+        for (const string& row : rows)
+            res += row;
+        return res;
+    }
+};
