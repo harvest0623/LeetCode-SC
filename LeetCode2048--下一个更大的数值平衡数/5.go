bb545564b1775fb18788736167c3a0d487898592
harvest0623 <3367741939@qq.com>
2026-01-31 16:08:57 +0800

    LeetCode2048

---
+++ LeetCode2048--下一个更大的数值平衡数/5.go
@@ -0,0 +1,22 @@
+func isBalance(x int) bool {
+    count := make([]int, 10)
+    for x > 0 {
+        count[x % 10]++
+        x /= 10
+    }
+    for i := 0; i < 10; i++ {
+        if count[i] > 0 && count[i] != i {
+            return false
+        }
+    }
+    return true
+}
+
+func nextBeautifulNumber(n int) int {
+    for i := n + 1; i <= 1224444; i++ {
+        if isBalance(i) {
+            return i
+        }
+    }
+    return -1
+}
