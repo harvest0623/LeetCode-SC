bfd98f4146df24d274dfa5305476d8d12c95ff15
harvest0623 <3367741939@qq.com>
2026-02-14 17:07:44 +0800

    LeetCode799

---
+++ LeetCode0799--香槟塔/1.js
@@ -0,0 +1,15 @@
+var champagneTower = function(poured, query_row, query_glass) {
+    let row = [poured];
+    for (let i = 1; i <= query_row; i++) {
+        const nextRow = new Array(i + 1).fill(0);
+        for (let j = 0; j < i; j++) {
+            const volume = row[j];
+            if (volume > 1) {
+                nextRow[j] += (volume - 1) / 2;
+                nextRow[j + 1] += (volume - 1) / 2;
+            }
+        }
+        row = nextRow;
+    }
+    return Math.min(1, row[query_glass]);
+};
