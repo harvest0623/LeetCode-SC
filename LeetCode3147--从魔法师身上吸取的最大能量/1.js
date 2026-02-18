8e6d522d4fdbc5d77a944155e8ab214fb08bd8ed
harvest0623 <3367741939@qq.com>
2026-02-18 21:19:43 +0800

    LeetCode3147

---
+++ LeetCode3147--从魔法师身上吸取的最大能量/1.js
@@ -0,0 +1,13 @@
+var maximumEnergy = function(energy, k) {
+    const n = energy.length;
+    let ans = -Number.MAX_SAFE_INTEGER;
+    
+    for (let i = n - k; i < n; i++) {
+        let sum = 0;
+        for (let j = i; j >= 0; j -= k) {
+            sum += energy[j];
+            ans = Math.max(ans, sum);
+        }
+    }
+    return ans;
+};
---
+++ LeetCode3147--从魔法师身上吸取的最大能量/6.rs
@@ -0,0 +1,22 @@
+impl Solution {
+    pub fn maximum_energy(energy: Vec<i32>, k: i32) -> i32 {
+        let n = energy.len();
+        let k_usize = k as usize;
+        let mut ans = i32::MIN;
+        for i in (n - k_usize)..n {
+            let mut sum = 0;
+            let mut j = i;
+            while j < n {
+                sum += energy[j];
+                if sum > ans {
+                    ans = sum;
+                }
+                if j < k_usize {
+                    break;
+                }
+                j -= k_usize;
+            }
+        }
+        ans
+    }
+}
