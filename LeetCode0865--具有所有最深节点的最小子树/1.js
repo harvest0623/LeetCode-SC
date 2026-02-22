ffd2c36572a3606f3309022f6a1b8b1a9bcd53b6
harvest0623 <3367741939@qq.com>
2026-02-22 18:51:10 +0800

    LeetCode865

--- LeetCode0865--具有所有最深节点的最小子树/1.js
+++ LeetCode0865--具有所有最深节点的最小子树/1.js
@@ -3,15 +3,15 @@ var subtreeWithAllDeepest = function(root) {
 };
 function dfs(root) {
     if (!root) {
-      return [0, root];
+        return [0, root];
     }
     let [d1, lca1] = dfs(root.left);
     let [d2, lca2] = dfs(root.right);
     if (d1 > d2) {
-      return [d1 + 1, lca1];
+        return [d1 + 1, lca1];
     }
     if (d1 < d2) {
-      return [d2 + 1, lca2];
+        return [d2 + 1, lca2];
     }
     return [d1 + 1, root];
 }
