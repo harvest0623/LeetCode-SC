4145b77874c1190361ba44332009f0bd0dd40398
harvest0623 <3367741939@qq.com>
2026-03-08 14:18:06 +0800

    LeetCode1545

---
+++ LeetCode1545--找出第 N 个二进制字符串中的第 K 位/1.js
@@ -0,0 +1,12 @@
+var findKthBit = function(n, k) {
+    if (n === 1) {
+        return '0';
+    }
+    if (k === 1 << (n - 1)) {
+        return '1';
+    }
+    if (k < 1 << (n - 1)) {
+        return findKthBit(n - 1, k);
+    }
+    return findKthBit(n - 1, (1 << n) - k) === '1' ? '0' : '1';
+};
